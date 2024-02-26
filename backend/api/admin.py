2c86d2648b303cb2355f3776ad5aceae3f525bfe
LAMOTTE <121647171+mouhamedbaba@users.noreply.github.com>
2024-02-26 01:07:20 +0000

    Delete backend directory

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(UserProfile)
-admin.site.register(Vehicle)
-admin.site.register(Reservation)
-admin.site.register(ReservationOption)
